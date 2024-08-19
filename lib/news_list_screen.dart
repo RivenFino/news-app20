4ba5e67482979c752b46517a06e1b6f45dfaae6f
RivenFino <btrcatdf07a5@gmail.com>
2024-08-19 14:37:56 +0800

    gradle fix

--- lib/news_list_screen.dart
+++ lib/news_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'pages/login_screen.dart';
 import 'add_edit_news_screen.dart';
 import 'news_model.dart';
 
